3f3e3ab1b965c4f39c508463f6b02e55dd0d1dd1
Evgeniy Cherednik <lifeotworld@gmail.com>
2023-02-01 17:18:31 +0300

    init viewBinding

--- app/src/main/java/com/vaiki/compositionnumber/presentation/WelcomeFragment.kt
+++ app/src/main/java/com/vaiki/compositionnumber/presentation/WelcomeFragment.kt
@@ -6,13 +6,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.vaiki.compositionnumber.R
+import com.vaiki.compositionnumber.databinding.FragmentWelcomeBinding
 
-class WelcomeFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
+
+    private var _binding: FragmentWelcomeBinding? = null
+    private val binding: FragmentWelcomeBinding
+        get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentWelcomeBinding.bind(view)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
